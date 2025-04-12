fc3981bdcbb19b11d8b9a83440dd4b6731c4a738
luka chikashvili <lukachikashvili2020@gmail.com>
2025-04-12 21:15:03 +0400

    add gray boxes below glovo bag

--- game/src/components/Experience.jsx
+++ game/src/components/Experience.jsx
@@ -1,22 +1,55 @@
 import { useGLTF } from '@react-three/drei'
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import { useFrame } from '@react-three/fiber'
 
-const Experience = () => {
-    const model = useGLTF('./glovo_backpack.glb');
+const HoverBag = ({ position = [0, 0, 0], model }) => {
+  const ref = useRef()
+  const [hovered, setHovered] = useState(false)
+
+  
+  useFrame(() => {
+    if (ref.current) {
+      const targetY = hovered ? 0.3 : 0
+      ref.current.position.y += (targetY - ref.current.position.y) * 0.1
+    }
+  })
+
+  
+
 
   return (
-   <>
-
-   {/* Center bag */}
-   <primitive object={model.scene.clone()} scale={3} rotation={[0, 3.5, 0]} position={[0, 0, 0]} />
-
-{/* Left bag */}
-<primitive object={model.scene.clone()} scale={3} rotation={[0, 3.5, 0]} position={[-3, 0, 0]} />
-
-{/* Right bag */}
-<primitive object={model.scene.clone()} scale={3} rotation={[0, 3.5, 0]} position={[3, 0, 0]} />
+    <group position={position}>
    
-   </>
+      <mesh
+        rotation={[-Math.PI / 2, 0, 0]}
+        position={[0, -1, 0]}
+        receiveShadow
+      >
+        <planeGeometry args={[1.5, 1.5]} />
+        <meshStandardMaterial color="gray" />
+      </mesh>
+
+    
+      <group
+        ref={ref}
+        onPointerOver={() => setHovered(true)}
+        onPointerOut={() => setHovered(false)}
+      >
+        <primitive object={model.scene.clone()} scale={3} rotation={[0, 3.5, 0]} />
+      </group>
+    </group>
+  )
+}
+
+const Experience = () => {
+  const model = useGLTF('./glovo_backpack.glb')
+
+  return (
+    <>
+      <HoverBag position={[-3, 0, 0]} color="lime" model={model} />
+      <HoverBag position={[0, 0, 0]} color="orange" model={model} />
+      <HoverBag position={[3, 0, 0]} color="skyblue" model={model} />
+    </>
   )
 }
 
