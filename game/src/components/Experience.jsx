0ad997b12b2fcca570c9874d2b36dca5612dac2e
luka chikashvili <lukachikashvili2020@gmail.com>
2025-04-12 22:13:29 +0400

    add drivers indian and georgian

--- game/src/components/Experience.jsx
+++ game/src/components/Experience.jsx
@@ -1,47 +1,47 @@
 import { Html, useGLTF } from '@react-three/drei'
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useFrame, useThree } from '@react-three/fiber'
 import * as THREE from 'three'
 
-const HoverBag = ({ position = [0, 0, 0], model, onSelect, index }) => {
-  const ref = useRef()
-  const [hovered, setHovered] = useState(false)
-  const [clicked, setClicked] = useState(false)
-
-  useFrame(() => {
-    if (ref.current) {
-      const targetY = hovered ? 0.3 : 0
-      ref.current.position.y += (targetY - ref.current.position.y) * 0.1
+const HoverBag = ({ position = [0, 0, 0], model, onSelect, index, driver }) => {
+    const ref = useRef()
+    const [hovered, setHovered] = useState(false)
+    const [clicked, setClicked] = useState(false)
+  
+    useFrame(() => {
+      if (ref.current) {
+        const targetY = hovered ? 0.3 : 0
+        ref.current.position.y += (targetY - ref.current.position.y) * 0.1
+      }
+    })
+  
+    const handlePlaneClick = () => {
+      setClicked(true)
+      onSelect(position, index, driver) 
     }
-  })
-
-  const handlePlaneClick = () => {
-    setClicked(true)
-    onSelect(position, index) 
-  }
-
-  return (
-    <group position={position}>
-      <mesh
-        rotation={[-Math.PI / 2, 0, 0]}
-        position={[0, -1, 0]}
-        receiveShadow
-        onClick={handlePlaneClick}
-      >
-        <planeGeometry args={[1.5, 1.5]} />
-        <meshStandardMaterial color={clicked ? '#169976' : 'gray'} />
-      </mesh>
-
-      <group
-        ref={ref}
-        onPointerOver={() => setHovered(true)}
-        onPointerOut={() => setHovered(false)}
-      >
-        <primitive object={model.scene.clone()} scale={3} rotation={[0, 3.5, 0]} />
+  
+    return (
+      <group position={position}>
+        <mesh
+          rotation={[-Math.PI / 2, 0, 0]}
+          position={[0, -1, 0]}
+          receiveShadow
+          onClick={handlePlaneClick}
+        >
+          <planeGeometry args={[1.5, 1.5]} />
+          <meshStandardMaterial color={clicked ? '#169976' : 'gray'} />
+        </mesh>
+  
+        <group
+          ref={ref}
+          onPointerOver={() => setHovered(true)}
+          onPointerOut={() => setHovered(false)}
+        >
+          <primitive object={model.scene.clone()} scale={3} rotation={[0, 3.5, 0]} />
+        </group>
       </group>
-    </group>
-  )
-}
+    )
+  }
 
 const Experience = () => {
   const model = useGLTF('./glovo_backpack.glb')
@@ -49,6 +49,19 @@ const Experience = () => {
   const targetPositionRef = useRef(null);
 
   const [numberModal, setNumberModal] = useState(false);
+
+  const [drivers, setDrivers] = useState([]);
+
+  const shuffleDrivers = () => {
+    const driverTypes = ['Georgian', 'Georgian', 'Indian']
+    driverTypes.sort(() => Math.random() - 0.5) 
+    setDrivers(driverTypes);
+    
+  }
+
+  useEffect(() => {
+    shuffleDrivers();
+  }, [])
 
   useFrame(() => {
     if (targetPositionRef.current) {
@@ -58,14 +71,15 @@ const Experience = () => {
     }
   })
 
-  const handleSelectBag = (bagPosition, index) => {
+  const handleSelectBag = (bagPosition, index, driver) => {
     targetPositionRef.current = new THREE.Vector3(...bagPosition)
     setNumberModal(index + 1);
+    console.log('Behind the bag is:', driver);
   }
 
   return (
     <>
-      <HoverBag position={[-3, 0, 0]} model={model} onSelect={handleSelectBag} index={0} />
+      <HoverBag position={[-3, 0, 0]} model={model} onSelect={handleSelectBag} driver={drivers[0]} index={0} />
       { (
   <Html center>
     <div className="numbermodal">
@@ -73,8 +87,8 @@ const Experience = () => {
     </div>
   </Html>
 )}
-      <HoverBag position={[0, 0, 0]} model={model} onSelect={handleSelectBag} index={1} />
-      <HoverBag position={[3, 0, 0]} model={model} onSelect={handleSelectBag} index={2} />
+      <HoverBag position={[0, 0, 0]} model={model} onSelect={handleSelectBag} index={1} driver={drivers[1]} />
+      <HoverBag position={[3, 0, 0]} model={model} onSelect={handleSelectBag} index={2} driver={drivers[2]} />
     </>
   )
 }
